cd808622ebb0edb81ac5d24d18276f40d19a38f5
Ben Hollander <ben@benhollander.com>
2024-12-07 11:24:49 -0600

    add fmt command

--- unusedLetters/index.ts
+++ unusedLetters/index.ts
@@ -1,16 +1,16 @@
-const FULL_ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const FULL_ALPHABET = "abcdefghijklmnopqrstuvwxyz".split("");
 
 export const findUnusedLetters = (s: string): string => {
-    if (typeof s !== 'string') {
-        throw new Error('Invalid input');
+  if (typeof s !== "string") {
+    throw new Error("Invalid input");
+  }
+
+  let unusedLetters = "";
+  FULL_ALPHABET.forEach((letter) => {
+    if (s.match(letter) === null) {
+      unusedLetters += letter;
     }
+  });
 
-    let unusedLetters = '';
-    FULL_ALPHABET.forEach(letter => {
-        if (s.match(letter) === null) {
-            unusedLetters += letter
-        }
-    })
-
-    return unusedLetters
-}
+  return unusedLetters;
+};
--- unusedLetters/spec.ts
+++ unusedLetters/spec.ts
@@ -1,8 +1,12 @@
-import { findUnusedLetters } from './index';
+import { findUnusedLetters } from "./index";
 
-test('unusedLetters', () => {
-    expect(findUnusedLetters('A slow yellow fox crawls under the proactive dog')).toBe('bjkmqz');
-    expect(findUnusedLetters('A quick brown fox jumps over the lazy dog')).toBe('');
-    //@ts-ignore
-    expect(() => findUnusedLetters({a: 'hi'})).toThrow('Invalid input');
-})
+test("unusedLetters", () => {
+  expect(
+    findUnusedLetters("A slow yellow fox crawls under the proactive dog"),
+  ).toBe("bjkmqz");
+  expect(findUnusedLetters("A quick brown fox jumps over the lazy dog")).toBe(
+    "",
+  );
+  //@ts-ignore
+  expect(() => findUnusedLetters({ a: "hi" })).toThrow("Invalid input");
+});
